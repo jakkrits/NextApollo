851f9e881471c8bd8b7246007715d98343af0bcc
Jakk Sut <jakks@Jakks-iMac.local>
2017-08-21 23:57:59 +0700

    rev favicon

--- pages/_document.js
+++ pages/_document.js
@@ -44,11 +44,29 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.3/css/bulma.min.css" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon/favicon-16x16.png"
+          />
           <link rel="manifest" href="/static/favicon/manifest.json" />
-          <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+          <meta
+            name="msapplication-config"
+            content="/static/favicon/browserconfig.xml"
+          />
           <meta name="theme-color" content="#ffffff" />
           {this.helmetHeadComponents()}
           {styleTags}
